7654b8155a3130246e0e3afbbb1465c9f4576d4d
targetc <codoreancatalin@yahoo.com>
2023-10-29 10:51:01 +0200

    nou

--- script.js
+++ script.js
@@ -15,3 +15,42 @@ function loveste()
     footer.classList.add("footeractiv");
 }
 
+/* clasa pentru sectiune activa = .sectiuneactiva */
+
+const inputElements = document.querySelectorAll('input, textarea');
+
+inputElements.forEach((input) =>
+{
+   input.addEventListener('focus', () =>
+   {
+    input.placeholder = '';
+   });
+
+   input.addEventListener('blur', () =>
+   {
+    if(input.value == '') 
+    {
+        input.placeholder = input.getAttribute('data-placeholder');
+    }
+   });
+   input.setAttribute('data-placeholder', input.placeholder);
+});
+
+
+/* tranzitii */
+
+const hiddenElements1 = document.querySelectorAll('.tranzitiehiddenstanga');
+const hiddenElements2 = document.querySelectorAll('.tranzitiehiddendreapta');
+const observer = new IntersectionObserver((entries) =>
+{
+    entries.forEach((entry) =>
+    {
+        if(entry.isIntersecting)
+        {
+            entry.target.classList.add('tranzitieshow');
+        }
+    });
+});
+
+hiddenElements1.forEach((el) => observer.observe(el));
+hiddenElements2.forEach((el) => observer.observe(el));
